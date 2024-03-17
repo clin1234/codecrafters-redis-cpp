e0109d8cf2191566def1f21434c9caaf2425a022
Charlie Lin <tuug@gmx.us>
2024-03-17 17:37:06 -0500

    try again

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,76 +1,78 @@
-#include <arpa/inet.h>
-#include <cstdlib>
-#include <cstring>
 #include <iostream>
-#include <netdb.h>
+#include <cstdlib>
 #include <string>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <thread>
+#include <cstring>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <thread>
 
-void recv_loop(int server_fd) {
+consteval unsigned BUFFER_SIZE = 1024;
+
+void handleClient(int client_fd)
+{
+  char command[BUFFER_SIZE];
+  char pong[] = "+PONG\r\n";
+  std::cout << "Client is: " << client_fd << '\n';
+  while(recv(client_fd, command, sizeof(command), 0))
+  {
+    send(client_fd, pong, strlen(pong), 0);
+  }
+  std::cout << "Client " << client_fd << " connection closed.\n";
+  close(client_fd);
+}
+
+void connectClient(int server_fd)
+{
   struct sockaddr_in client_addr;
   int client_addr_len = sizeof(client_addr);
-
+  
   std::cout << "Waiting for a client to connect...\n";
-  int client_fd = accept(server_fd, (struct sockaddr *)&client_addr,
-                  (socklen_t *)&client_addr_len);
-
-  std::cout << "Client connected\n";
-  char buffer[1024];      // Buffer to store received data
-  ssize_t bytes_received; // Variable to store the number of bytes received
-
-  while ((bytes_received = recv(client_fd, buffer, sizeof(buffer), 0)) >
-         0) { // Receive data in a loop
-    send(client_fd, "+PONG\r\n", 7,
-         0); // Respond with +PONG\r\n for each received command
-  }
+    int conn_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+    std::cout << "Client connected: " << conn_fd << std::endl;
+    std::jthread(handleClient, conn_fd);
 }
 
 int main(int argc, char **argv) {
-  // You can use print statements as follows for debugging, they'll be visible
-  // when running tests. std::cout << "Logs from your program will appear
-  // here!\n";
-
-  // Uncomment this block to pass the first stage
-
+  // You can use print statements as follows for debugging, they'll be visible when running tests.
+  std::cout << "Logs from your program will appear here!\n";
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0) {
-    std::cerr << "Failed to create server socket\n";
-    return 1;
+   std::cerr << "Failed to create server socket\n";
+   return 1;
   }
-
-  // Since the tester restarts your program quite often, setting SO_REUSEADDR
+  
+  // Since the tester restarts your program quite often, setting REUSE_PORT
   // ensures that we don't run into 'Address already in use' errors
   int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) <
-      0) {
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
     std::cerr << "setsockopt failed\n";
     return 1;
   }
-
+  
   struct sockaddr_in server_addr;
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = INADDR_ANY;
   server_addr.sin_port = htons(6379);
-
-  if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) !=
-      0) {
+  
+  if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
     std::cerr << "Failed to bind to port 6379\n";
     return 1;
   }
-
+  
   int connection_backlog = 5;
   if (listen(server_fd, connection_backlog) != 0) {
     std::cerr << "listen failed\n";
     return 1;
   }
-
-  std::jthread r1(recv_loop, server_fd);
-  std::jthread r2(recv_loop, server_fd);
-
-  //close(client_fd);
-  close(server_fd);
   
+  while (true)
+  {
+    connectClient(server_fd);
+  }
+  
+  close(server_fd);
+  return 0;
 }
